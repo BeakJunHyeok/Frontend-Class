4f3b16261097eed564f67afb5d499b6732a57a82
BeakJunHyeok <wnsgur1832@naver.com>
2024-07-09 18:21:26 +0900

    240709

---
+++ 240709/script.js
@@ -0,0 +1,13 @@
+const circle = document.querySelector("#circle");
+console.log(circle);
+const article = circle.querySelectorAll("article");
+
+article.forEach((el) => {
+  el.addEventListener("mouseenter", () => {
+    circle.style.animationPlayState = "paused";
+  });
+
+  el.addEventListener("mouseleave", () => {
+    circle.style.animationPlayState = "running";
+  });
+});
