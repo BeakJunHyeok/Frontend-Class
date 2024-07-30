e1a5dd3c132c538cfd5c1b85b919fb5a0f4dc020
BeakJunHyeok <wnsgur1832@naver.com>
2024-07-30 09:28:11 +0900

    240729

---
+++ 240729/01js/script.js
@@ -0,0 +1,37 @@
+// const test1 = "mon";
+// test1 = "tue";
+
+//프로그램을 개발
+// - width 1920px
+// - 슬라이드 개수 5개 고정
+
+// 스크롤을 내리게 되었을 때, 내려간 값에 따라서 무언가를 변경!! > let
+
+// let test2 = "mon";
+// test2 = "tue";
+
+//var x = 10;
+//var z = x+y;
+//var y = 20;
+
+// NAN = Not a Number => 자료의 형태 중 하나
+
+// const num1 = 1;
+// const num2 = 2;
+// const sum = num1 + num2;
+
+// console.log(typeof sum);
+
+// const str1 = "1";
+// const str2 = "2";
+// console.log(typeof str1);
+
+const num = 1000;
+const str = "원";
+const counter = num + str;
+console.log(typeof counter);
+
+const num1 = 1000;
+const str1 = "2000";
+const counter1 = num1 + str1;
+console.log(typeof counter1);
