5951e4a2df0beb60d59305cf8822ffbd4925bf2b
BeakJunHyeok <wnsgur1832@naver.com>
2024-08-20 18:22:06 +0900

    240820

---
+++ 240820/01js/script2.js
@@ -0,0 +1,20 @@
+const result = document.querySelector("#result");
+
+const member1 = ["HTML", "Node", "React"];
+const member2 = ["CSS", "Javascript", "React"];
+const member3 = ["Javascript", "React"];
+
+const subjects = [...member1, ...member2, ...member3];
+const resultList = new Set();
+
+subjects.forEach((subject) => {
+  resultList.add(subject);
+});
+
+console.log(resultList);
+
+result.innerHTML = `
+<ul>
+  ${[...resultList].map((subject) => `<li>${subject}</li>`).join("")}
+</ul>
+`;
