f68a4a21309e6149818108886f955c12a3c20f50
BeakJunHyeok <wnsgur1832@naver.com>
2024-11-05 16:37:53 +0900

    241105

--- 241101/01react-ts/src/components/Board.tsx
+++ 241101/01react-ts/src/components/Board.tsx
@@ -1,7 +1,10 @@
 import { Droppable } from "react-beautiful-dnd";
+import { useForm } from "react-hook-form";
 import DraggableCard from "./DraggableCard";
+import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
-
+import React, { useRef } from "react";
+import { ToDo, toDoState } from "../atoms";
 const Wrapper = styled.div`
   background: ${(props) => props.theme.boardColor};
   padding: 20px 10px;
@@ -18,23 +21,102 @@ const Title = styled.h2`
   text-transform: uppercase;
 `;
 
+interface AreaProps {
+  isDraggingOver: boolean;
+  isDraggingFromThis: boolean;
+}
+
+const Area = styled.div<AreaProps>`
+  /* background: dodgerblue; */
+  width: 100%;
+  background: ${(props) =>
+    props.isDraggingOver
+      ? "pink"
+      : props.isDraggingFromThis
+      ? "crimson"
+      : "dodgerblue"};
+  padding: 10px;
+  border-radius: 8px;
+  transition: background 0.3s;
+`;
+
+const Form = styled.form`
+  width: 100%;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 8px;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+`;
+
 interface BoardProps {
-  toDos: string[];
+  toDos: ToDo[];
   boardId: string;
 }
 
+interface FormProps {
+  toDo: string;
+}
+
 const Board = ({ toDos, boardId }: BoardProps) => {
+  const setToDos = useSetRecoilState(toDoState);
+  const { register, setValue, handleSubmit } = useForm<FormProps>();
+  const onVaild = ({ toDo }: FormProps) => {
+    const newTodo = {
+      id: Date.now(),
+      text: toDo,
+    };
+    setToDos((allBoards) => {
+      return {
+        ...allBoards,
+        [boardId]: [newTodo, ...allBoards[boardId]],
+      };
+    });
+    setValue("toDo", "");
+  };
+  // const inputRef = useRef<HTMLInputElement>(null);
+  // const onClick = () => {
+  //   inputRef.current?.focus();
+  //   setTimeout(() => {
+  //     inputRef.current?.blur();
+  //   }, 5000);
+  // };
   return (
     <Wrapper>
+      {/* <input ref={inputRef} type="text" placeholder="Please..." />
+      <button onClick={onClick}>Click</button> */}
       <Title>{boardId}</Title>
+      <Form onSubmit={handleSubmit(onVaild)}>
+        <Input
+          {...register("toDo", { required: true })}
+          type="text"
+          placeholder={`Add Task On ${boardId}`}
+        />
+      </Form>
       <Droppable droppableId={boardId}>
-        {(magic) => (
-          <div ref={magic.innerRef} {...magic.droppableProps}>
+        {(magic, snapshot) => (
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+            ref={magic.innerRef}
+            {...magic.droppableProps}
+          >
             {toDos.map((toDo, index) => (
-              <DraggableCard toDo={toDo} key={toDo} index={index} />
+              <DraggableCard
+                toDoId={toDo.id}
+                toDoText={toDo.text}
+                key={toDo.id}
+                index={index}
+              />
             ))}
             {magic.placeholder}
-          </div>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
